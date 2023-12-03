8bd12d47288717208aa2cb753e001c2c4a4de870
Max Goad <maxdgoad@gmail.com>
2023-12-03 16:50:45 -0600

    Day 4 setup

---
+++ Advent2023/Advent4x1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using AdventOfCode.Utils;
+
+namespace AdventOfCode.Advent2023
+{
+    internal class Advent4x1
+    {
+        public static string Run()
+        {
+            var stringArr = FileReader.ReadFile("Advent4.txt");
+
+            var ans = 0;
+
+            return ans.ToString();
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ Stopwatch stopwatch = new Stopwatch();
 
 stopwatch.Start();
 
-var output = Advent3x2.Run();
+var output = Advent4x1.Run();
 
 Console.WriteLine($"Answer: {output}");
 
