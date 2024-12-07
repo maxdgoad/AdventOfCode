9ae0910d5b8cfcafd4f75dfdbaf2e515b1951b34
Max Goad <maxdgoad@gmail.com>
2024-12-07 13:08:09 -0600

    Day 8 prep and .net9

---
+++ Avent2024/Advent8x2.cs
@@ -0,0 +1,15 @@
+﻿using AdventOfCode.Utils;
+using System.Numerics;
+
+namespace AdventOfCode.Advent2024;
+internal class Advent8x2
+{
+    public static string Run()
+    {
+        var stringArr = FileReader.ReadFile("Advent8Example.txt").Select((str) => str[0]).ToList();
+
+        var ans = 0;
+
+        return ans.ToString();
+    }
+}
