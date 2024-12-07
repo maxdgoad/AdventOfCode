dd72ce5906b96ecc62147267ccd20c4abc6aad2f
Max Goad <maxdgoad@gmail.com>
2024-12-07 13:04:03 -0600

    Clean file reader

--- Utils/FileReader.cs
+++ Utils/FileReader.cs
@@ -1,56 +1,60 @@
 ﻿namespace AdventOfCode.Utils;
 internal static class FileReader
 {
-    static string MaxPath = "C:\\Users\\maxdg\\OneDrive\\Desktop\\AdventOfCode\\TextFiles2024\\";
+    static string FilePath = "\\TextFiles2024\\";
     public static List<List<string>> ReadFile(string fileName, string splitter = "")
     {
         var response = new List<List<string>>();
+        var currentDirectory = Directory.GetCurrentDirectory();
         string? line;
         try
         {
-            //Pass the file path and file name to the StreamReader constructor
-            StreamReader sr = new StreamReader(MaxPath + fileName);
-            //Read the first line of text
+            // Pass the file path and file name to the StreamReader constructor
+            StreamReader sr = new StreamReader(currentDirectory + FilePath + fileName);
+            // Read the first line of text
             line = sr.ReadLine();
-            //Continue to read until you reach end of file
+            // Continue to read until you reach end of file
             while (line != null)
             {
                 response.Add(line.Split(splitter).Where(str => !string.IsNullOrEmpty(str)).Select(str => str.Trim()).ToList());
                 line = sr.ReadLine();
             }
-            //close the file
+            // Close the file
             sr.Close();
         }
         catch (Exception e)
         {
             Console.WriteLine("Exception: " + e.Message);
         }
+
         return response;
     }
 
     public static List<List<char>> ReadFileCharArray(string fileName, string splitter = "")
     {
         var response = new List<List<char>>();
+        var currentDirectory = Directory.GetCurrentDirectory();
         string? line;
         try
         {
-            //Pass the file path and file name to the StreamReader constructor
-            StreamReader sr = new StreamReader(MaxPath + fileName);
-            //Read the first line of text
+            // Pass the file path and file name to the StreamReader constructor
+            StreamReader sr = new StreamReader(currentDirectory + FilePath + fileName);
+            // Read the first line of text
             line = sr.ReadLine();
-            //Continue to read until you reach end of file
+            // Continue to read until you reach end of file
             while (line != null)
             {
                 response.Add(line.ToCharArray().ToList());
                 line = sr.ReadLine();
             }
-            //close the file
+            // Close the file
             sr.Close();
         }
         catch (Exception e)
         {
             Console.WriteLine("Exception: " + e.Message);
         }
+
         return response;
     }
 }
