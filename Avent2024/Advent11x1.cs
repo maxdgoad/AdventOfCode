850b6ee553b60efd3dc2465453b37eafa69751c6
Max Goad <maxdgoad@gmail.com>
2024-12-11 00:14:39 -0600

    Day 11

---
+++ Avent2024/Advent11x1.cs
@@ -0,0 +1,116 @@
+﻿using AdventOfCode.Utils;
+using System.Numerics;
+
+namespace AdventOfCode.Advent2024;
+internal class Advent11x1
+{
+    public class Node
+    {
+        public BigInteger value;
+        public Node? left;
+        public Node? right;
+        public bool isLeaf = true;
+
+    }
+    public static string Run()
+    {
+        var stringArr = FileReader.ReadFile("Advent11.txt", " ")[0].ToList();
+
+        var ans = 0;
+
+        var nodes = new List<Node>();
+
+        foreach (var c in stringArr)
+        {
+            var node = new Node();
+            node.value = int.Parse(c);
+            
+            nodes.Add(node);
+        }
+
+        foreach (var node in nodes)
+        {
+            BuildTree(node, 25);
+        }
+
+        // count the leaves
+        foreach (var node in nodes)
+        {
+            ans += CountLeaves(node);  
+            //PrintLeaves(node);
+        }
+
+        return ans.ToString();
+    }
+
+    public static void BuildTree(Node? node, int depth)
+    {
+        if (depth == 0)
+        {
+            return;
+        }
+        if (node == null)
+        { 
+            return;
+        }
+        if (node.value == 0)
+        {
+            node.value = 1;
+            BuildTree(node, depth - 1);
+        }
+        else if (node.value.ToString().Length % 2 == 0)
+        {
+            var str = node.value.ToString();
+            var left = int.Parse(str[..(str.Length / 2)]);
+
+            var right = int.Parse(str[(str.Length / 2)..]);
+
+            var leftNode = new Node();
+            leftNode.value = left;
+            leftNode.isLeaf = true;
+
+            var rightNode = new Node();
+            rightNode.value = right;
+            rightNode.isLeaf = true;
+
+            node.left = leftNode;
+            node.right = rightNode;
+            node.isLeaf = false;
+
+            BuildTree(node.left, depth - 1);
+            BuildTree(node.right, depth - 1);
+        }
+        else
+        {
+            node.value *= 2024;
+            BuildTree(node, depth - 1);
+        }
+    }
+
+    public static int CountLeaves(Node? node)
+    {
+        if (node == null)
+        {
+            return 0;
+        }
+        if (node.isLeaf)
+        {
+            return 1;
+        }
+        return CountLeaves(node.left) + CountLeaves(node.right);
+    }
+
+    public static void PrintLeaves(Node? node)
+    {
+        if (node == null)
+        {
+            return;
+        }
+        if (node.isLeaf)
+        {
+            Console.Write(node.value + " ");
+        }
+        PrintLeaves(node.left);
+        PrintLeaves(node.right);
+    }
+}
---
+++ Avent2024/Advent11x2.cs
@@ -0,0 +1,91 @@
+﻿using AdventOfCode.Utils;
+using System.Numerics;
+
+namespace AdventOfCode.Advent2024;
+internal class Advent11x2
+{
+    public class Node
+    {
+        public BigInteger value;
+        public Node? left;
+        public Node? right;
+        public bool isLeaf = true;
+    }
+
+    public static int MAX_DEPTH = 75;
+
+    public static string Run()
+    {
+        var stringArr = FileReader.ReadFile("Advent11.txt", " ")[0].ToList();
+
+        BigInteger ans = 0;
+
+        var nodes = new List<Node>();
+
+        foreach (var c in stringArr)
+        {
+            var node = new Node();
+            node.value = int.Parse(c);
+            nodes.Add(node);
+        }
+
+        var memo = new Dictionary<(string, int), BigInteger>();
+        foreach (var node in nodes)
+        {
+            ans += BuildTree(node, MAX_DEPTH, memo);
+        }
+
+        return ans.ToString();
+    }
+
+    public static BigInteger BuildTree(Node node, int depth, Dictionary<(string, int), BigInteger> memo)
+    {
+        if (memo.TryGetValue((node.value.ToString(), depth), out var me))
+        {
+            return me;
+        }
+        if (depth == 0)
+        {
+            return 1;
+        }
+        if (node.value == 0)
+        {
+            var val = node.value.ToString();
+            node.value = 1;
+            var m = BuildTree(node, depth - 1, memo);
+            memo[(val, depth)] = m;
+            return m;
+        }
+        else if (node.value.ToString().Length % 2 == 0)
+        {
+            var str = node.value.ToString();
+            var left = int.Parse(str[..(str.Length / 2)]);
+
+            var right = int.Parse(str[(str.Length / 2)..]);
+
+            var leftNode = new Node();
+            leftNode.value = left;
+            leftNode.isLeaf = true;
+
+            var rightNode = new Node();
+            rightNode.value = right;
+            rightNode.isLeaf = true;
+
+            node.left = leftNode;
+            node.right = rightNode;
+            node.isLeaf = false;
+
+            var m = BuildTree(node.left, depth - 1, memo) + BuildTree(node.right, depth - 1, memo); 
+            memo[(str, depth)] = m;
+            return m;
+        }
+        else
+        {
+            var val = node.value.ToString();
+            node.value *= 2024;
+            var m = BuildTree(node, depth - 1, memo);
+            memo[(val, depth)] = m;
+            return m;
+        }
+    }
+}
