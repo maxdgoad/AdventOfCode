0ccab907bcb55e096c08ef25c53428169c5e84b5
Max Goad <maxdgoad@gmail.com>
2024-12-14 16:54:08 -0600

    Day 14

---
+++ Avent2024/Advent14x1.cs
@@ -0,0 +1,106 @@
+﻿using AdventOfCode.Utils;
+using System.Numerics;
+
+namespace AdventOfCode.Advent2024;
+internal class Advent14x1
+{
+    public class Robot
+    {
+        public (int, int) Position { get; set; }
+        public (int, int) Velocity { get; set; }
+    }
+
+    public static string Run()
+    {
+        var stringArr = FileReader.ReadFile("Advent14.txt").Select((str) => str.Count == 0 ? "" : str[0]).ToList();
+
+        BigInteger ans = 0;
+
+        var robots = new List<Robot>();
+
+        foreach (var str in stringArr)
+        {
+            var firstComma = str.IndexOf(',');
+            var x = int.Parse(str.Substring(2, firstComma-2));
+
+            var velocityIndex = str.IndexOf('v');
+            var y = int.Parse(str.Substring(firstComma + 1, velocityIndex - firstComma - 1));
+
+            var secondComma = str.IndexOf(',', velocityIndex);
+
+            var xVel = int.Parse(str.Substring(velocityIndex + 2, secondComma - velocityIndex - 2));
+            var yVel = int.Parse(str.Substring(secondComma + 1, str.Length - secondComma - 1));
+
+            var robot = new Robot();
+            robot.Position = (x, y);
+            robot.Velocity = (xVel, yVel);
+            robots.Add(robot);
+        }
+
+        var dimX = 101;
+        var dimY = 103;
+        for (int t = 0; t < 100; t++)
+        {
+            foreach (var robot in robots)
+            {
+                //if (robot.Velocity.Item1 == 2 && robot.Velocity.Item2 == -3)
+                //{
+                //    Console.WriteLine($"({robot.Position.Item1}, {robot.Position.Item2}) t={t}, ({robot.Position.Item1 % dimX}, {robot.Position.Item2 % dimY})");
+                //}
+                var newX = robot.Position.Item1 + robot.Velocity.Item1;
+                var newY = robot.Position.Item2 + robot.Velocity.Item2;
+
+                if (newX < 0)
+                {
+                    newX = dimX + newX;
+                }
+
+                if (newY < 0)
+                {
+                    newY = dimY + newY;
+                }
+
+                robot.Position = (newX, newY);
+            }
+        }
+        var quadrant1 = 0;
+        var quadrant2 = 0;
+        var quadrant3 = 0;
+        var quadrant4 = 0;
+        foreach (var robot in robots)
+        {
+            var posX = robot.Position.Item1 % dimX;
+            var posY = robot.Position.Item2 % dimY;
+
+            if (posX != (dimX - 1) / 2 && posY != (dimY - 1) / 2)
+            {
+                if (posX < (dimX - 1) / 2)
+                {
+                    if (posY < (dimY - 1) / 2)
+                    {
+                        quadrant1++;
+                    }
+                    else
+                    {
+                        quadrant3++;
+                    }
+                }
+                else
+                {
+                    if (posY < (dimY - 1) / 2)
+                    {
+                        quadrant2++;
+                    }
+                    else
+                    {
+                        quadrant4++;
+                    }
+                }
+            }
+        }
+
+        ans = quadrant1 * quadrant2 * quadrant3 * quadrant4;
+
+        return ans.ToString();
+    }
+}
---
+++ Avent2024/Advent14x2.cs
@@ -0,0 +1,141 @@
+﻿using AdventOfCode.Utils;
+using System.Numerics;
+
+namespace AdventOfCode.Advent2024;
+internal class Advent14x2
+{
+    public class Robot
+    {
+        public (int, int) InitialPosition { get; set; }
+        public int Recurrence { get; set; }
+        public (int, int) Position { get; set; }
+        public (int, int) Velocity { get; set; }
+        public int TreeStep { get; set; }
+    }
+
+    public static string Run()
+    {
+        var stringArr = FileReader.ReadFile("Advent14.txt").Select((str) => str.Count == 0 ? "" : str[0]).ToList();
+
+        BigInteger ans = 0;
+
+        var robots = new List<Robot>();
+
+        foreach (var str in stringArr)
+        {
+            var firstComma = str.IndexOf(',');
+            var x = int.Parse(str.Substring(2, firstComma - 2));
+
+            var velocityIndex = str.IndexOf('v');
+            var y = int.Parse(str.Substring(firstComma + 1, velocityIndex - firstComma - 1));
+
+            var secondComma = str.IndexOf(',', velocityIndex);
+
+            var xVel = int.Parse(str.Substring(velocityIndex + 2, secondComma - velocityIndex - 2));
+            var yVel = int.Parse(str.Substring(secondComma + 1, str.Length - secondComma - 1));
+
+            var robot = new Robot();
+            robot.InitialPosition = (x, y);
+            robot.Recurrence = 0;
+            robot.TreeStep = 0;
+            robot.Position = (x, y);
+            robot.Velocity = (xVel, yVel);
+            robots.Add(robot);
+        }
+
+        var dimX = 101;
+        var dimY = 103;
+        var halfX = (dimX - 1) / 2;
+
+        var treePositions = new HashSet<(int, int)>();
+
+        treePositions.Add((halfX, 0));
+        for (int y = 1; y < (dimX-1)/2; y++)
+        {
+            treePositions.Add((halfX - y, y));
+            treePositions.Add((halfX + y, y));
+        }
+
+        double lowXVar = 1000;
+        double lowYVar = 1000;
+
+        for (int t = 1; t <= 10404; t++)
+        {
+            var positions = new Dictionary<(int, int), int>();
+            foreach (var robot in robots)
+            {
+                var newX = robot.Position.Item1 + robot.Velocity.Item1;
+                var newY = robot.Position.Item2 + robot.Velocity.Item2;
+
+                if (newX < 0)
+                {
+                    newX = dimX + newX;
+                }
+
+                if (newY < 0)
+                {
+                    newY = dimY + newY;
+                }
+                robot.Position = (newX % dimX, newY % dimY);
+
+                if (robot.Position == robot.InitialPosition && robot.Recurrence == 0)
+                {
+                    robot.Recurrence = t;
+                }
+
+                if (robot.Recurrence == 0 && treePositions.Contains(robot.Position))
+                {
+                    robot.TreeStep = t;
+                }
+
+                if (positions.ContainsKey(robot.Position))
+                {
+                    positions[robot.Position]++;
+                }
+                else
+                {
+                    positions[robot.Position] = 1;
+                }
+            }
+
+            var meanX = positions.Keys.Select(k => k.Item1).Average();
+            var meanY = positions.Keys.Select(k => k.Item2).Average();
+
+            var varianceX = positions.Keys.Select(k => k.Item1).Select(x => (x - meanX) * (x - meanX)).Sum() / positions.Keys.Count;
+            var varianceY = positions.Keys.Select(k => k.Item2).Select(y => (y - meanY) * (y - meanY)).Sum() / positions.Keys.Count;
+
+            var shouldPrint = varianceX < lowXVar && varianceY < lowYVar;
+            if (shouldPrint)
+            {
+                lowXVar = varianceX;
+                lowYVar = varianceY;
+
+                Console.WriteLine($"t={t}, varX={varianceX}, varY={varianceY}");
+                for (int y = 0; y < dimY; y++)
+                {
+                    for (int x = 0; x < dimX; x++)
+                    {
+                        if (positions.ContainsKey((x, y)))
+                        {
+                            Console.Write(positions[(x, y)]);
+                        }
+                        else
+                        {
+                            Console.Write(".");
+                        }
+                    }
+                    Console.WriteLine();
+                }
+                Console.WriteLine();
+                Console.WriteLine();
+            }   
+        }
+
+        //foreach (var robot in robots)
+        //{
+        //    Console.WriteLine($"Robot at {robot.InitialPosition} tree={robot.TreeStep} and recurrence={robot.Recurrence}");   
+        //}
+
+        return ans.ToString();
+    }
+}
