87d6f1e9865f8289fd682f1bea917ffd7e745620
Max Goad <maxdgoad@gmail.com>
2024-12-20 15:44:06 -0600

    Day 17

---
+++ Avent2024/Advent17x1.cs
@@ -0,0 +1,114 @@
+﻿using AdventOfCode.Utils;
+using System.Numerics;
+using System.Text;
+
+namespace AdventOfCode.Advent2024;
+internal class Advent17x1
+{
+    public static int rA = 0;
+    public static int rB = 0;
+    public static int rC = 0;
+
+    public static int ComboOperand(int operand)
+    {
+        if (operand <= 3)
+        {
+            return operand;
+        }
+        if (operand == 4)
+        {
+            return rA;
+        }
+        if (operand == 5)
+        {
+            return rB;
+        }
+        if (operand == 6)
+        {
+            return rC;
+        }
+        return 0;
+    }
+    public static string Run()
+    {
+        var stringArr = FileReader.ReadFile("Advent17.txt");
+
+        BigInteger ans = 0;
+
+        rA = int.Parse(stringArr[0][0].Substring(12));
+        rB = int.Parse(stringArr[1][0].Substring(12));
+        rC = int.Parse(stringArr[2][0].Substring(12));
+
+        var instructions = stringArr[4][0].Substring(8).Split(",").Select(str => int.Parse(str)).ToArray();
+
+        var output = new StringBuilder();
+        try
+        {
+            var instrPtr = 0;
+
+            while (instrPtr < instructions.Length-1)
+            {
+                var instr = instructions[instrPtr];
+
+                var operand = instructions[instrPtr + 1];
+
+                var doesJump = false;
+                if (instr == 0)
+                {
+                    var numerator = rA;
+                    var denominator = (int)Math.Pow(2, ComboOperand(operand));
+                    rA = numerator / denominator;
+                }
+                else if (instr == 1)
+                {
+                    rB = rB ^ operand;
+                }
+                else if (instr == 2)
+                {
+                    rB = ComboOperand(operand) % 8;
+                }
+                else if (instr == 3)
+                {
+                    if (rA != 0)
+                    {
+                        instrPtr = operand;
+                        doesJump = true;
+                    }
+                }
+                else if (instr == 4)
+                {
+                    rB = rB ^ rC;
+                }
+                else if (instr == 5)
+                {
+                    output.Append((ComboOperand(operand) % 8) + ",");
+                    Console.WriteLine("Register A: " + rA);
+
+                }
+                else if (instr == 6)
+                {
+                    var numerator = rA;
+                    var denominator = (int)Math.Pow(2, ComboOperand(operand));
+                    rB = numerator / denominator;
+                }
+                else if (instr == 7)
+                {
+                    var numerator = rA;
+                    var denominator = (int)Math.Pow(2, ComboOperand(operand));
+                    rC = numerator / denominator;
+                }
+                if (!doesJump)
+                {
+                    instrPtr += 2;
+                }
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine("Exception: " + e.Message);
+        }
+
+        return output.ToString();
+    }
+
+}
---
+++ Avent2024/Advent17x2.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Utils;
+using System.Numerics;
+
+namespace AdventOfCode.Advent2024;
+internal class Advent17x2
+{
+    public static string Run()
+    {
+        BigInteger ans = 0;
+
+        var values = new List<int>() { 2, 4, 1, 3, 7, 5, 0, 3, 1, 4, 4, 7, 5, 5, 3, 0 };
+        BigInteger previous = 0;
+        for (int k = values.Count - 1; k >= 0; k--)
+        {
+            for (BigInteger i = 8 * previous; i < 9 * previous + 10; i++)
+            {
+                var a0 = i;
+                var lhs = (((a0 % 8) ^ 3) ^ 4);
+                var rhs = a0 / BigInteger.Pow(2, (int)(a0 % 8) ^ 3);
+                if ((lhs ^ rhs) % 8 == values[k] )
+                {
+                    if (i / 8 == previous)
+                    {
+                        Console.WriteLine(values[k] + ": " + i);
+                        previous = i;
+                        break;
+                    }          
+                }
+            }
+        }
+
+        return ans.ToString();
+    }
+}
