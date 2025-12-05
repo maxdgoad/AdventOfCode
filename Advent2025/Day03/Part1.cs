a2d85fbe869a5b5362450aa52a9f7b6d452acaf1
maxdgoad <maxdgoad@gmail.com>
2025-12-05 12:58:34 -0600

    Day 3, both parts

--- Advent2025/Day03/Part1.cs
+++ Advent2025/Day03/Part1.cs
@@ -6,9 +6,34 @@ internal class Part1
 {
     public static string Run()
     {
-        var stringArr = FileReader.ReadFile("Day03\\test.txt", " ");
-
+        var stringArr = FileReader.ReadFile("Day03\\advent.txt", " ");
+        var banks = stringArr.Select(x => x[0].Select(c =>
+        {
+            var s = c.ToString();
+            return int.Parse(s);
+        }).ToList()).ToList();
         var ans = 0;
+
+        foreach (var bank in banks)
+        {
+            var maxTens = 0;
+            var maxOnes = 0;
+            var max = 0;
+            for (int i = 0; i < bank.Count; i++)
+            {
+                for (int j = bank.Count - 1; j > i; j--)
+                {
+                    if (bank[i] * 10 + bank[j] > max)
+                    {
+                        maxTens = bank[i];
+                        maxOnes = bank[j];
+                        max = bank[i] * 10 + bank[j];
+                    }
+                }
+            }
+            Console.WriteLine($"Max tens: {maxTens}, Max ones: {maxOnes}, Max: {max}");
+            ans += max;
+        }
 
         return ans.ToString();
     }
--- Advent2025/Day03/Part2.cs
+++ Advent2025/Day03/Part2.cs
@@ -6,10 +6,38 @@ internal class Part2
 {
     public static string Run()
     {
-        var stringArr = FileReader.ReadFile("Day03\\test.txt", " ");
+        var stringArr = FileReader.ReadFile("Day03\\advent.txt", " ");
+        var banks = stringArr.Select(x => x[0].Select(c => int.Parse(c.ToString())).ToList()).ToList();
+        long ans = 0;
 
-        var ans = 0;
+        foreach (var bank in banks)
+        {
+            var max = GetMaxRecursive(-1, 12, bank);
+            ans += max;
+
+            Console.WriteLine($"Max: {max}");
+            memo.Clear();
+        }
 
         return ans.ToString();
+    }
+
+    private static Dictionary<(int, int), long> memo = new();
+    public static long GetMaxRecursive(int startIndex, int place, List<int> bank)
+    {
+        if (place == 0) return 0;
+
+        if (memo.ContainsKey((startIndex, place))) return memo[(startIndex, place)];
+
+        long max = 0;
+        for (int i = bank.Count - place; i > startIndex; i--)
+        {
+            var currentMax = bank[i] * (long)Math.Pow(10, place - 1) + GetMaxRecursive(i, place - 1, bank);
+            if (currentMax > max) max = currentMax;
+        }
+
+        memo[(startIndex, place)] = max;
+
+        return max;
     }
 }
